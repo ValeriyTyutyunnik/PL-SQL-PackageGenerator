bf835a0085688ca8946d644e2b5c47b058ebddad
Valeriy Tyutyunnik <valeriytyutyunnik@yandex.ru>
2020-01-07 14:37:23 +0300

    add licence

--- pkg_gen__package.sql
+++ pkg_gen__package.sql
@@ -1,6 +1,30 @@
 -- encoding Windows-1251
+-- https://github.com/ValeriyTyutyunnik/PL-SQL-PackageGenerator
 
 create or replace package pkg_gen as
+  /*
+    MIT License
+
+    Copyright (c) 2020 Valeriy Tyutyunnik
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+  */
 
   -- define the prefix and suffix that should be part of the parameter/variable name
   param_prefix  varchar2(5 char) := 'p_';
